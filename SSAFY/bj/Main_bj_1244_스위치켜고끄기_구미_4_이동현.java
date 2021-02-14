737d0e0c29511afec0719866ca7c62c7801343c1
이동현 <ehdgus4166@gmail.com>
2021-02-14 17:01:15 +0900

    bj_1244_스위치켜고끄기

--- SSAFY/bj/Main_bj_1244_스위치켜고끄기_구미_4_이동현.java
+++ SSAFY/bj/Main_bj_1244_스위치켜고끄기_구미_4_이동현.java
@@ -5,51 +5,45 @@ import java.io.*;
 
 public class Main_bj_1244_스위치켜고끄기_구미_4_이동현 {
 	static int num, stu;
-	static int[] stat;
+	static int[] swit;
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
 		num = sc.nextInt() + 1;
-		stat = new int[num];
+		swit = new int[num];
 		
 		for (int i = 1; i < num; i++) {
-			stat[i] = sc.nextInt();
+			swit[i] = sc.nextInt();
 		}
-		
 		stu = sc.nextInt();
 		for (int i = 1; i <= stu; i++) {
 			int sex = sc.nextInt();
 			int key = sc.nextInt();
 			
 			if (sex == 1) {
-				for (int j = key; j < num; j+=key) {
-					stat[j]^=1;
+				for (int j = key; j < num; j++) {
+					if (j % key == 0) change(j);
 				}
-			} else  {
-				int left = key - 1;
-				int right = key + 1;
-				
-				while (true) {
-					if (left < 1 || right >= num) break;
-					if (stat[left] != stat[right]) {
-						break;
-					}
-					left--;
-					right++;
-				}
-				left++;
-				right--;
-				while(left<=right) {
-					stat[left]^=1;
-					left++;
+			}
+			else {
+				int n = 1;
+				change(key);
+				while (n < swit.length/2) {
+					if (key + n < swit.length && key - n >= 1 && swit[key+n] == swit[key - n]) {
+						change(key+n);
+						change(key-n);
+					} else break;
+					n++;
 				}
 			}
 		}
-		for (int j = 1; j< num; j++) {
-			System.out.print(stat[j] + " ");
-			if(j%20 == 0) System.out.println();
+		for (int i = 1; i < swit.length;i++) {
+			System.out.print(swit[i] + " ");
+			if (i%20 == 0) System.out.println();
 		}
-		
 	}
-
+	static void change(int idx) {
+		if (swit[idx] == 0) swit[idx] = 1;
+		else swit[idx] = 0;
+	}
 }
