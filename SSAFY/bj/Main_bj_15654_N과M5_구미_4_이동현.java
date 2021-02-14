c861cbe96f559d7df442f63dfb156e49181fdbcf
이동현 <ehdgus4166@gmail.com>
2021-02-14 12:26:10 +0900

    bj_15654_N과M5

--- SSAFY/bj/Main_bj_15654_N과M5_구미_4_이동현.java
+++ SSAFY/bj/Main_bj_15654_N과M5_구미_4_이동현.java
@@ -9,6 +9,7 @@ import java.util.StringTokenizer;
 public class Main_bj_15654_N과M5_구미_4_이동현 {
 	static StringBuilder sb;
 	static int[] input;
+	static int[] nums;
 	static boolean[] visit;
 	static int N;
 	static int M;
@@ -20,6 +21,7 @@ public class Main_bj_15654_N과M5_구미_4_이동현 {
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 		input = new int[N];
+		nums = new int[N];
 		visit = new boolean[N];
 		
 		st = new StringTokenizer(br.readLine());
@@ -27,23 +29,19 @@ public class Main_bj_15654_N과M5_구미_4_이동현 {
 		for (int i = 0; i < N; i++) input[i] = Integer.parseInt(st.nextToken());
 		Arrays.sort(input);
 		subset(0);
-		System.out.println(sb);
 	}
-	
 	static void subset(int cnt) {
-		if (cnt == N) {
-			int len = 0;
-			for (int i = 0; i < visit.length; i++) if (visit[i]) sb.append(input[i] + " ");;
-			/*if (len == M) {
-				for (int i = 0; i < N; i++) {
-					if (visit[i]) sb.append(input[i] + " ");
-				}
-				sb.append("\n");
-			}*/
+		if (cnt == M) {
+			for (int i = 0; i < M; i++)  System.out.print(nums[i] + " ");
+			System.out.println();
 			return;
 		}
-		visit[cnt] = true;
-		subset(cnt+1);
+		for (int i = 0; i < input.length; i++) {
+			if (visit[i]) continue;
+			visit[i] = true;
+			nums[cnt] = input[i];
+			subset(cnt+1);
+			visit[i] = false;
+		}
 	}
-
 }
