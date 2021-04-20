d0e8691c02212d202298014accb4ba7e2394d48a
이동현 <ehdgus4166@gmail.com>
2021-04-20 20:13:53 +0900

    bj_15666_N과M12

---
+++ SSAFY/bj/Main_bj_15666_N과M12_구미_4_이동현.java
@@ -0,0 +1,43 @@
+package com.ssafy.bj;
+import java.util.*;
+import java.io.*;
+
+public class Main_bj_15666_N과M12_구미_4_이동현 {
+	static int N, M;
+	static int[] nums;
+	static StringBuilder sb;
+	static ArrayList<Integer> list;
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = stoi(st.nextToken()); M = stoi(st.nextToken());
+		nums = new int[M];
+		sb = new StringBuilder();
+		st = new StringTokenizer(br.readLine());
+		Set<Integer> set = new TreeSet<>();
+		for (int i = 0; i < N; i++) {
+			int num = stoi(st.nextToken());
+			set.add(num);
+		}
+		list = new ArrayList<>(set);
+		permu(0, 0);
+		System.out.println(sb);
+	}
+	
+	static void permu(int idx, int start) {
+		if (idx == M) {
+			for (int i = 0; i < M; i++) {
+				sb.append(nums[i]).append(" ");
+			}
+			sb.append("\n");
+			return;
+		}
+		for (int i = start; i < list.size(); i++) {
+			nums[idx] = list.get(i);
+			permu(idx+1, i);
+		}
+	}
+	static int stoi(String s) {
+		return Integer.parseInt(s);
+	}
+}
